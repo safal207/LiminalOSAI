4e107d18fcfa20ca6e00bf444e08b1d9ba68cfcc
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2025-10-14 20:44:31 +0300

    Fix broadcast delivery on resonant bus

--- bus/resonant.c
+++ bus/resonant.c
@@ -3,9 +3,12 @@
 #include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <string.h>
 
 static resonant_msg bus_queue[RESONANT_BUS_CAPACITY];
+static uint64_t broadcast_delivery_mask[RESONANT_BUS_CAPACITY];
 static size_t bus_count = 0;
 
 static int sensor_registry[RESONANT_BUS_CAPACITY];
@@ -26,15 +29,19 @@ static bool sensor_registered(int sensor_id)
     return false;
 }
 
-static void queue_duplicate(const resonant_msg *msg, int sensor_id)
+static int sensor_index_of(int sensor_id)
 {
-    if (!msg || sensor_id == RESONANT_BROADCAST_ID) {
-        return;
+    if (sensor_id == RESONANT_BROADCAST_ID) {
+        return -1;
     }
 
-    resonant_msg duplicate = *msg;
-    duplicate.target_id = sensor_id;
-    bus_emit(&duplicate);
+    for (size_t i = 0; i < sensor_count; ++i) {
+        if (sensor_registry[i] == sensor_id) {
+            return (int)i;
+        }
+    }
+
+    return -1;
 }
 
 void bus_register_sensor(int sensor_id)
@@ -58,6 +65,7 @@ static void shift_right_from(size_t index)
 {
     for (size_t i = bus_count; i > index; --i) {
         bus_queue[i] = bus_queue[i - 1];
+        broadcast_delivery_mask[i] = broadcast_delivery_mask[i - 1];
     }
 }
 
@@ -65,12 +73,14 @@ static void shift_left_from(size_t index)
 {
     for (size_t i = index; i + 1 < bus_count; ++i) {
         bus_queue[i] = bus_queue[i + 1];
+        broadcast_delivery_mask[i] = broadcast_delivery_mask[i + 1];
     }
 }
 
 void bus_init(void)
 {
     memset(bus_queue, 0, sizeof(bus_queue));
+    memset(broadcast_delivery_mask, 0, sizeof(broadcast_delivery_mask));
     bus_count = 0;
     memset(sensor_registry, 0, sizeof(sensor_registry));
     sensor_count = 0;
@@ -88,10 +98,14 @@ void bus_emit(const resonant_msg *msg)
             return;
         }
         bus_queue[last] = *msg;
+        broadcast_delivery_mask[last] = 0ULL;
         while (last > 0 && bus_queue[last - 1].energy < bus_queue[last].energy) {
             resonant_msg tmp = bus_queue[last - 1];
             bus_queue[last - 1] = bus_queue[last];
             bus_queue[last] = tmp;
+            uint64_t mask_tmp = broadcast_delivery_mask[last - 1];
+            broadcast_delivery_mask[last - 1] = broadcast_delivery_mask[last];
+            broadcast_delivery_mask[last] = mask_tmp;
             --last;
         }
         return;
@@ -104,6 +118,7 @@ void bus_emit(const resonant_msg *msg)
 
     shift_right_from(insert_index);
     bus_queue[insert_index] = *msg;
+    broadcast_delivery_mask[insert_index] = 0ULL;
     ++bus_count;
 }
 
@@ -115,24 +130,48 @@ bool bus_listen(int sensor_id, resonant_msg *out_msg)
 
     bus_register_sensor(sensor_id);
 
+    int registry_index = sensor_index_of(sensor_id);
+
     for (size_t i = 0; i < bus_count; ++i) {
-        if (bus_queue[i].target_id == sensor_id || bus_queue[i].target_id == RESONANT_BROADCAST_ID) {
-            resonant_msg message = bus_queue[i];
+        resonant_msg message = bus_queue[i];
+        if (message.target_id == sensor_id) {
             shift_left_from(i);
             --bus_count;
+            broadcast_delivery_mask[bus_count] = 0ULL;
             *out_msg = message;
-
-            if (message.target_id == RESONANT_BROADCAST_ID) {
-                for (size_t sensor_index = 0; sensor_index < sensor_count; ++sensor_index) {
-                    int registered_id = sensor_registry[sensor_index];
-                    if (registered_id == sensor_id) {
-                        continue;
-                    }
-                    queue_duplicate(&message, registered_id);
-                }
-            }
             return true;
         }
+
+        if (message.target_id != RESONANT_BROADCAST_ID || registry_index < 0) {
+            continue;
+        }
+
+        uint64_t delivered_mask = broadcast_delivery_mask[i];
+        uint64_t sensor_bit = 1ULL << (uint64_t)registry_index;
+        if (delivered_mask & sensor_bit) {
+            continue;
+        }
+
+        delivered_mask |= sensor_bit;
+        broadcast_delivery_mask[i] = delivered_mask;
+        *out_msg = message;
+
+        bool all_delivered = true;
+        for (size_t sensor_index = 0; sensor_index < sensor_count; ++sensor_index) {
+            uint64_t bit = 1ULL << (uint64_t)sensor_index;
+            if ((delivered_mask & bit) == 0ULL) {
+                all_delivered = false;
+                break;
+            }
+        }
+
+        if (all_delivered) {
+            shift_left_from(i);
+            --bus_count;
+            broadcast_delivery_mask[bus_count] = 0ULL;
+        }
+
+        return true;
     }
 
     return false;
