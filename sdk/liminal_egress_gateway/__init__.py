7f0b9d54ccfd945fc5296b0bc51d9661330166b0
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-08-07 09:49:51 +0300

    feat: export egress gateway API

---
+++ sdk/liminal_egress_gateway/__init__.py
@@ -0,0 +1,27 @@
+from .gateway import (
+    ALLOWED_METHODS,
+    GATEWAY_AUTHORITY,
+    RECEIPT_SCHEMA,
+    DirectSocketGuard,
+    EgressBlocked,
+    EgressError,
+    EgressGateway,
+    GatewayRequest,
+    NetworkExecutionReceipt,
+    TransportRequest,
+    TransportResponse,
+)
+
+__all__ = [
+    "ALLOWED_METHODS",
+    "GATEWAY_AUTHORITY",
+    "RECEIPT_SCHEMA",
+    "DirectSocketGuard",
+    "EgressBlocked",
+    "EgressError",
+    "EgressGateway",
+    "GatewayRequest",
+    "NetworkExecutionReceipt",
+    "TransportRequest",
+    "TransportResponse",
+]
